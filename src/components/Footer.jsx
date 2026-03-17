ae21aad1e3f4ff4627fa09ed3bb58592784b2e4e
niki2422 <randazzonico55@gmail.com>
2026-03-17 11:19:36 -0300

    se creo component footer y whatsappButton, con su respectivo css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css"
+
+export default function Footer(){
+
+return(
+
+<footer className="footer">
+
+<h3>CompuReact</h3>
+
+<p>Bahía Blanca</p>
+<p>Av. Colón 1234</p>
+<p>Tel: 291-555-0000</p>
+
+</footer>
+
+)
+
+}
---
+++ src/components/WhatsappButton.jsx
@@ -0,0 +1,21 @@
+import "./WhatsappButton.css"
+
+export default function WhatsappButton(){
+
+return(
+
+<a
+href="https://wa.me/5492915555555"
+target="_blank"
+className="whatsapp"
+>
+
+<img
+src="https://cdn-icons-png.flaticon.com/512/733/733585.png"
+/>
+
+</a>
+
+)
+
+}
