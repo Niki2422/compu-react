408175f42f097a9958c1d6c9aa05439a0117e9c4
niki2422 <randazzonico55@gmail.com>
2026-05-19 15:43:47 -0300

    se controller y route para creacion de producto

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   getProducts,
   getProduct,
+  createNewProduct,
 } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.get("/", getProducts);
 router.get("/:id", getProduct);
+router.post("/", createNewProduct);
 
 export default router;
